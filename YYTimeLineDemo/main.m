19abc2282e5bcb6942e3bee715a56d89640752c3
mrzhang1219@163.com <lovezhu359605>
2017-07-04 10:17:52 +0800

    Initial Commit

---
+++ YYTimeLineDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YYTimeLineDemo
+//
+//  Created by Ryan on 2017/7/4.
+//  Copyright © 2017年 Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
